641320f6ffa5bea1bcf44bd40eac3e6d4b4f623a
toku-sa-n <42336339+toku-sa-n@users.noreply.github.com>
2019-11-29 21:58:55 +0900

    [Add]Support for one question symbol

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,23 @@ fn main() {
 }
 
 fn decompress_regex(regex: &str) -> Vec<String> {
-    vec![regex.to_string()]
+    let mut decompressed_strings: Vec<String> = vec![String::from("")];
+
+    let current_idx = 0;
+    let mut num_kinds = 1;
+    for c in regex.chars() {
+        if c == '?' {
+            decompressed_strings.push(decompressed_strings[current_idx].clone());
+            num_kinds += 1;
+
+            decompressed_strings[current_idx + 1].pop();
+        } else {
+            for idx in 0..num_kinds {
+                decompressed_strings[idx].push(c);
+            }
+        }
+    }
+    decompressed_strings
 }
 
 #[cfg(test)]
@@ -14,5 +30,14 @@ mod tests {
     fn test_string_with_no_regex_symbols() {
         assert_eq!(vec!["hello, world!"], decompress_regex("hello, world!"));
         assert_eq!(vec!["This is a pen."], decompress_regex("This is a pen."));
+    }
+
+    #[test]
+    fn test_string_with_one_question_symbol() {
+        assert_eq!(vec!["hello!", "hello"], decompress_regex("hello!?"));
+        assert_eq!(
+            vec!["hello! world", "hello world"],
+            decompress_regex("hello!? world")
+        );
     }
 }
