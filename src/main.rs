34020b3a0c1a58bb8b98ee9a0966eca5af96518f
toku-sa-n <42336339+toku-sa-n@users.noreply.github.com>
2019-11-29 23:47:16 +0900

    [Add]Decompress sentences with multple ? symbols

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ fn main() {
 fn decompress_regex(regex: &str) -> Vec<String> {
     let mut decompressed_strings: Vec<String> = vec![String::from("")];
 
-    let current_idx = 0;
     let mut num_kinds = 1;
     for c in regex.chars() {
         if c == '?' {
-            decompressed_strings.push(decompressed_strings[current_idx].clone());
-            num_kinds += 1;
+            for i in 0..num_kinds {
+                decompressed_strings.push(decompressed_strings[i].clone());
+            }
+            num_kinds *= 2;
 
-            decompressed_strings[current_idx + 1].pop();
+            for i in num_kinds / 2..num_kinds {
+                decompressed_strings[i].pop();
+            }
         } else {
             for idx in 0..num_kinds {
                 decompressed_strings[idx].push(c);
@@ -32,12 +35,26 @@ mod tests {
         assert_eq!(vec!["This is a pen."], decompress_regex("This is a pen."));
     }
 
+    // TODO: Test for only one character with `?'.
     #[test]
     fn test_string_with_one_question_symbol() {
         assert_eq!(vec!["hello!", "hello"], decompress_regex("hello!?"));
         assert_eq!(
             vec!["hello! world", "hello world"],
             decompress_regex("hello!? world")
+        );
+    }
+
+    // TODO: Make wrapper of assert_eq! which doesn't consider order of elements.
+    #[test]
+    fn test_string_with_multiple_question_symbols() {
+        assert_eq!(
+            vec!["Yahoo!", "ahoo!", "Yhoo!", "hoo!"],
+            decompress_regex("Y?a?hoo!")
+        );
+        assert_eq!(
+            vec!["abc", "bc", "ac", "c", "ab", "b", "a", ""],
+            decompress_regex("a?b?c?")
         );
     }
 }
