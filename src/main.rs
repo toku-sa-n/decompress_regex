3765a664fc505dbf77c8e697d82e5067ca609b6e
toku-sa-n <42336339+toku-sa-n@users.noreply.github.com>
2019-11-29 21:26:42 +0900

    [Add]Implement for the sentence with no symbols

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn decompress_regex(regex: &str) -> Vec<String> {
+    vec![regex.to_string()]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_string_with_no_regex_symbols() {
+        assert_eq!(vec!["hello, world!"], decompress_regex("hello, world!"));
+        assert_eq!(vec!["This is a pen."], decompress_regex("This is a pen."));
+    }
+}
